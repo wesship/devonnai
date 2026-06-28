e036091bf08b5fb60f879330854bee9a208e56fc
wesship <37167955+wesship@users.noreply.github.com>
2026-06-28 12:17:09 +0000

    Add optional analytics script support

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,11 +1,24 @@
 import "@/styles/globals.css";
 import type { AppProps } from "next/app";
+import Script from "next/script";
 import Layout from "@/components/layout/Layout";
 
 export default function App({ Component, pageProps }: AppProps) {
+  const plausibleDomain = process.env.NEXT_PUBLIC_PLAUSIBLE_DOMAIN;
+
   return (
-    <Layout>
-      <Component {...pageProps} />
-    </Layout>
+    <>
+      {plausibleDomain ? (
+        <Script
+          defer
+          data-domain={plausibleDomain}
+          src="https://plausible.io/js/script.js"
+          strategy="afterInteractive"
+        />
+      ) : null}
+      <Layout>
+        <Component {...pageProps} />
+      </Layout>
+    </>
   );
 }
